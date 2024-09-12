e062e675c4f4c3d8a3a102d477f738dc7b40ae02
saada2006 <58188800+saada2006@users.noreply.github.com>
2024-09-12 00:50:58 -0400

    KPCNN doesn't work???? What????

--- nnref/cnnref.py
+++ nnref/cnnref.py
@@ -4,62 +4,52 @@ import torch.nn.functional as F
 
 print("Hello World, this language is very unintuitive")
 
-class CnnTest(nn.Module):
+class KpcnnTest(nn.Module):
     def __init__(self):
-        super(CnnTest, self).__init__()
+        super(KpcnnTest, self).__init__()
 
         self.numLayers = 4
+        self.numWeights = 8
 
         self.conv = list()
         for layer in range(self.numLayers):
-            curconv = nn.Conv2d(8, 8, 3, padding=1)
+            inchnl = 8
+            outchnl =8
 
-            rawkernel = list()
-            for och in range(8):
-                singlechannel = list()
-                for ich in range(8):
-                    singlechannel.append([
-                        [1.0 / 9.0, 1.0 / 9.0, 1.0 / 9.0],
-                        [1.0 / 9.0, 1.0 / 9.0, 1.0 / 9.0],
-                        [1.0 / 9.0, 1.0 / 9.0, 1.0 / 9.0],
-                    ])
-                rawkernel.append(singlechannel)
+            if(layer == 0):
+                inchnl = 8
+            elif(layer == self.numLayers - 1):
+                outchnl = 8
 
-            with torch.no_grad():
-                curconv.weight.data = torch.tensor(rawkernel)
-
+            curconv = nn.Conv2d(inchnl, outchnl, 3, padding=1)
             self.conv.append(curconv)
 
+        # 4 illum channels
+        self.postconv = nn.Conv2d(8, 4 * self.numWeights, 5)
 
+        self.smax = nn.Softmax(dim=0)
+        self.kmax = nn.Softmax2d()
     def forward(self, input):
         # apply convolution
         convmaps = input
 
-        if(False):
-            for layer in range(self.numLayers):
-                print(self.conv[layer].weight.data[7][7][2][2].item())
-                convmaps = F.relu(self.conv[layer](convmaps))
-        else:
-            for layer in range(self.numLayers):
-                curconv = nn.Conv2d(8, 8, 3, padding=1)
+        for layer in range(self.numLayers):
+            convmaps = F.relu(self.conv[layer](convmaps))
 
-                rawkernel = list()
-                for och in range(8):
-                    singlechannel = list()
-                    for ich in range(8):
-                        singlechannel.append([
-                            [1.0 / 9.0, 1.0 / 9.0, 1.0 / 9.0],
-                            [1.0 / 9.0, 1.0 / 9.0, 1.0 / 9.0],
-                            [1.0 / 9.0, 1.0 / 9.0, 1.0 / 9.0],
-                        ])
-                    rawkernel.append(singlechannel)
 
-                convmaps = F.relu(F.conv2d(convmaps, torch.tensor(rawkernel), padding=1))
 
-        return convmaps
+        #normPostconv = self.kmax(self.postconv.weight.data)
+        postconvcolors = self.postconv(input).view(4, self.numWeights)
+        normWeights = self.smax(convmaps)
+        filtered = torch.Tensor(4, 5, 5)
 
-cnntest = CnnTest()
+        for y in range(5):
+            for x in range(5):
+                filtered[:, y, x] = torch.matmul(postconvcolors, normWeights[:, y, x])
 
+        return filtered
+
+kpcnntest = KpcnnTest()
 
 rawdata = [
     [
@@ -78,6 +68,7 @@ rawdata = [
     ],
 ]
 
+# two more illum channels, then 4 channels more for normal + depth
 for blankmap in range(0, 6):
     rawdata.append([
         [0.0, 0.0, 0.0, 0.0, 0.0,],
@@ -87,29 +78,51 @@ for blankmap in range(0, 6):
         [0.0, 0.0, 0.0, 0.0, 0.0,],
     ])
 
-t = torch.tensor(rawdata)
+targetdata = list()
+for channel in range(4):
+    targetdata.append(rawdata[channel])
 
-o = cnntest.forward(t)
+inputtensor = torch.tensor(rawdata)
+targettensor = torch.tensor(targetdata)
 
-print(o.size())
+#o = cnntest.forward(t)
+#print(o.size())
 
-for y in range(5):
-    for x in range(5):
-        ppRWList = list()
+crit = nn.L1Loss()
+optim = torch.optim.SGD(kpcnntest.parameters(), lr=0.001, momentum=0.9)
+
+for epoch in range(200):
+    optim.zero_grad()
+
+    outputtensor = kpcnntest(inputtensor)
+    loss = crit(outputtensor, targettensor)
+    loss.backward()
+    optim.step()
+
+    print(loss.item())
+
+    if(epoch == 0 or epoch == 199):
+        print(outputtensor)
 
 
-        for w in range(8):
-            ppRWList.append(o[w][y][x].item())
-            #print(o[w][y][x].item(), end="\t")
+if False:
+    for y in range(5):
+        for x in range(5):
+            ppRWList = list()
 
-        ppRW = torch.tensor(ppRWList);
 
-        if False:
-            ppW = nn.Softmax()(ppRW)
-        else:
-            ppW = ppRW
+            for w in range(8):
+                ppRWList.append(o[w][y][x].item())
+                #print(o[w][y][x].item(), end="\t")
 
-        print(ppW)
+            ppRW = torch.tensor(ppRWList);
+
+            if True:
+                ppW = nn.Softmax()(ppRW)
+            else:
+                ppW = ppRW
+
+            print(ppW)
 
 
 
