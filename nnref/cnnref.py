4ca5315832789068bd7120963ad8caa9f8844f37
saada2006 <58188800+saada2006@users.noreply.github.com>
2024-09-14 21:52:09 -0400

    At least semi working kpcnn in pytorch?

--- nnref/cnnref.py
+++ nnref/cnnref.py
@@ -3,49 +3,76 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 print("Hello World, this language is very unintuitive")
+torch.set_printoptions(precision=4, sci_mode=False)
 
 class KpcnnTest(nn.Module):
     def __init__(self):
         super(KpcnnTest, self).__init__()
 
-        self.numLayers = 4
-        self.numWeights = 8
+        self.doposenc = False
 
-        self.conv = list()
+        self.numLayers = 1
+        self.numWeights = 12
+
+
+        self.conv = nn.ParameterList()
         for layer in range(self.numLayers):
-            inchnl = 8
-            outchnl =8
+            inchnl = 25
+            outchnl = 25
 
             if(layer == 0):
-                inchnl = 8
-            elif(layer == self.numLayers - 1):
-                outchnl = 8
+                if(self.doposenc):
+                    inchnl = 6
+                else:
+                    inchnl = 4
+            if(layer == self.numLayers - 1):
+                outchnl = self.numWeights
 
             curconv = nn.Conv2d(inchnl, outchnl, 3, padding=1)
             self.conv.append(curconv)
 
         # 4 illum channels
-        self.postconv = nn.Conv2d(8, 4 * self.numWeights, 5)
+        self.postconv = nn.Conv2d(4, 4 * self.numWeights, 5)
 
         self.smax = nn.Softmax(dim=0)
-        self.kmax = nn.Softmax2d()
     def forward(self, input):
-        # apply convolution
+
         convmaps = input
 
+
+        if(self.doposenc):
+            posenc = list()
+
+            for blankmap in range(0, 2):
+                posenc.append([
+                    [0.0, 0.0, 0.0, 0.0, 0.0,],
+                    [0.0, 0.0, 0.0, 0.0, 0.0,],
+                    [0.0, 0.0, 0.0, 0.0, 0.0,],
+                    [0.0, 0.0, 0.0, 0.0, 0.0,],
+                    [0.0, 0.0, 0.0, 0.0, 0.0,],
+                ])
+
+            for y in range(5):
+                for x in range(5):
+                    posenc[0][y][x] = y - 2
+                    posenc[1][y][x] = x - 2
+
+            convmaps = torch.cat((convmaps, torch.Tensor(posenc)), 0)
+
+        # apply convolution
         for layer in range(self.numLayers):
             convmaps = F.relu(self.conv[layer](convmaps))
 
 
 
-        #normPostconv = self.kmax(self.postconv.weight.data)
         postconvcolors = self.postconv(input).view(4, self.numWeights)
-        normWeights = self.smax(convmaps)
+        normWeights = convmaps#F.softmax(convmaps, dim=0)
         filtered = torch.Tensor(4, 5, 5)
 
         for y in range(5):
             for x in range(5):
                 filtered[:, y, x] = torch.matmul(postconvcolors, normWeights[:, y, x])
+                #print(normWeights[:, y, x])
 
         return filtered
 
@@ -69,7 +96,7 @@ rawdata = [
 ]
 
 # two more illum channels, then 4 channels more for normal + depth
-for blankmap in range(0, 6):
+for blankmap in range(0, 2):
     rawdata.append([
         [0.0, 0.0, 0.0, 0.0, 0.0,],
         [0.0, 0.0, 0.0, 0.0, 0.0,],
@@ -77,6 +104,10 @@ for blankmap in range(0, 6):
         [0.0, 0.0, 0.0, 0.0, 0.0,],
         [0.0, 0.0, 0.0, 0.0, 0.0,],
     ])
+
+
+
+print(rawdata)
 
 targetdata = list()
 for channel in range(4):
@@ -88,10 +119,19 @@ targettensor = torch.tensor(targetdata)
 #o = cnntest.forward(t)
 #print(o.size())
 
+count_dict = dict()
+
+for name, param in kpcnntest.named_parameters():
+    #if param.requires_grad:
+        #print(name, param.data)
+    count_dict[name] = 0
+
+
 crit = nn.L1Loss()
 optim = torch.optim.SGD(kpcnntest.parameters(), lr=0.001, momentum=0.9)
 
-for epoch in range(200):
+num_training_iter = 1000
+for epoch in range(num_training_iter):
     optim.zero_grad()
 
     outputtensor = kpcnntest(inputtensor)
@@ -99,10 +139,18 @@ for epoch in range(200):
     loss.backward()
     optim.step()
 
+    for name, param in kpcnntest.named_parameters():
+        if 'weight' in name:
+            temp = torch.zeros(param.grad.shape)
+            temp[param.grad != 0] += 1
+            count_dict[name] += temp
+
     print(loss.item())
 
-    if(epoch == 0 or epoch == 199):
+    if(epoch == 0 or epoch == num_training_iter - 1):
         print(outputtensor)
+
+#print(count_dict)
 
 
 if False:
