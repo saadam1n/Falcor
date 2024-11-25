1f78fb7287f02012ca16df382314a40747f210ba
saada2006 <58188800+saada2006@users.noreply.github.com>
2024-11-25 12:58:43 -0500

    Adjust parameters for Simple Kernel

--- prototyping/simple_kernel.py
+++ prototyping/simple_kernel.py
@@ -7,10 +7,8 @@ class SimpleKernel(nn.Module):
         super().__init__()
 
         # RGB in, RGB out, 7x7 kernel
-        self.conv1 = nn.Conv2d(3, 3, 13, padding=6)
+        self.conv1 = nn.Conv2d(12, 3, 13, padding=6)
         nn.init.constant_(self.conv1.weight, 1.0 / (13.0 * 13.0))
 
     def forward(self, input):
         return self.conv1(input)
-
-
